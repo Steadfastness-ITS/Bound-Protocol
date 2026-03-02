e9d37bc3dae755c157be85c73db0f6a5a9a02e79
Steadydrumz <steadydrums14@gmail.com>
2026-03-02 18:37:12 +0100

    Fix: force app container height to auto

--- src/App.jsx
+++ src/App.jsx
@@ -34,7 +34,7 @@ function App() {
   // }, [isMenuOpen]);
 
   return (
-    <div className="max-w-[1440px] mx-auto relative">
+    <div className="max-w-[1440px] mx-auto relative h-auto">
       <header className="relative z-[1000] bg-white">
         
         <DesktopLayout 
